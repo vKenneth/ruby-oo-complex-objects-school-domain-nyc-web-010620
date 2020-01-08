6f2238dffc0a664d466bc4dbd9336e09396fc013
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-08 14:25:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+school = School.new("Memorial High School")
+def initialize(roster)
+  @roster = roster
+end
+
+school.add_student("Zach Morris", 9)
+school.roster
+hash["new_key"] = []
+hash["new array"] << "new_value_for_value_array"
+
+hash => {"new_key"=> ["new_value_for_value_array"]}
+ 
+ school.add_student << ("AC Slater", 9)
+ school.add_student << ("Kelly Kapowski", 10)
+ school.add_student << ("Screech", 11)
+ school.roster
+ 
+ school.grade(9)
+ school.sort
