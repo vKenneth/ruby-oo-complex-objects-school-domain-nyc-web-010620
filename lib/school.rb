94ba143ec947145ee9cde2fc4cc28a879774ff57
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-08 14:26:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,7 @@ school.roster
 hash["new_key"] = []
 hash["new array"] << "new_value_for_value_array"
 
-hash => {"new_key"=> ["new_value_for_value_array"]}
+hash => {"new_key" => ["new_value_for_value_array"]}
  
  school.add_student << ("AC Slater", 9)
  school.add_student << ("Kelly Kapowski", 10)
