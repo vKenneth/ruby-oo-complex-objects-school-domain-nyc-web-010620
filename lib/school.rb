032ba774160f556128229913757b3c9366dbf254
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-08 14:38:13 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+school = School.new("Bayside High School")
+def initialize(roster)
+  @roster = roster
+end
+
+school.add_student("Zach Morris")
+school.roster
+hash["new_key"] = []
+hash["new array"] << "new_value_for_value_array"
+
+hash {"new_key" ["new_value_for_value_array"]}
+ 
+ school.add_student ("AC Slater")
+ school.add_student ("Kelly Kapowski")
+ school.add_student ("Screech")
+ school.roster
+ 
+ school.grade(9)
+ school.sort
+
+school.add_student_grade ("Zack Morris")
+school.add_student_grade ("AC Slater")
+school.add_student_grade ("Kelly Kapowski")
+school.add_student_grade ("Screech")
+
