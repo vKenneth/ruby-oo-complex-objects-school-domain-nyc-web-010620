2ab47d6b856c26994315d4e09f7deac869b6bf50
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-08 14:29:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,17 +3,18 @@ def initialize(roster)
   @roster = roster
 end
 
-school.add_student("Zach Morris" 9)
+school.add_student("Zach Morris")
 school.roster
 hash["new_key"] = []
 hash["new array"] << "new_value_for_value_array"
 
 hash {"new_key" ["new_value_for_value_array"]}
  
- school.add_student ("AC Slater" 9)
- school.add_student ("Kelly Kapowski" 10)
- school.add_student ("Screech" 11)
+ school.add_student ("AC Slater")
+ school.add_student ("Kelly Kapowski")
+ school.add_student ("Screech")
  school.roster
  
  school.grade(9)
  school.sort
+ school.add.
